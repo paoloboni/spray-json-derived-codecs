4ec9bf46472be2ca33ef82a3991073719b3a6cdd
Paolo Boni <boni.paolo@gmail.com>
2021-01-09 17:13:18 +0000

    Update headers

--- src/main/scala/spray/json/derived/derived.scala
+++ src/main/scala/spray/json/derived/derived.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Paolo Boni
+ * Copyright 2021 Paolo Boni
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
