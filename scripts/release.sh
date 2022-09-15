7a967c94fded590920126ac120cf395a423c7fe0
Paolo Boni <4746479+paoloboni@users.noreply.github.com>
2022-09-15 22:50:06 +0100

    New release process

--- scripts/release.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-: "${SONATYPE_USERNAME?must be defined}"
-: "${SONATYPE_PASSWORD?must be defined}"
-
-GPG_TTY=$(tty)
-export GPG_TTY
-sbt "release cross with-defaults skip-tests"
