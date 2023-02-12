1742153b7830d4dc12760b11e3f7234342410e88
Paolo Boni <4746479+paoloboni@users.noreply.github.com>
2023-02-12 15:16:13 +0000

    Update scalafmt-core to 3.7.1 (#129)
    * Update scalafmt-core to 3.7.1
    
    * Reformat with scalafmt 3.7.1
    
    Executed command: scalafmt --non-interactive
    
    * Add 'Reformat with scalafmt 3.7.1' to .git-blame-ignore-revs
    
    * Revert commit(s) 89bbd9c, b03545c, 424b7ec
    
    * Update scalafmt-core to 3.7.1
    
    * Reformat with scalafmt 3.7.1
    
    Executed command: scalafmt --non-interactive
    
    * Add 'Reformat with scalafmt 3.7.1' to .git-blame-ignore-revs

--- src/test/scala-2/spray/json/derived/DerivedCodecsSpec.scala
+++ src/test/scala-2/spray/json/derived/DerivedCodecsSpec.scala
@@ -42,11 +42,12 @@ class DerivedCodecsSpec
       import spray.json.derived.auto._
 
       case class Cat(name: String, livesLeft: Int)
-      forAll { cat: Cat =>
-        checkRoundTrip[Cat](
-          cat,
-          s"""{"name": "${cat.name}", "livesLeft": ${cat.livesLeft}}"""
-        )
+      forAll {
+        cat: Cat =>
+          checkRoundTrip[Cat](
+            cat,
+            s"""{"name": "${cat.name}", "livesLeft": ${cat.livesLeft}}"""
+          )
       }
     }
 
@@ -55,11 +56,12 @@ class DerivedCodecsSpec
       import spray.json.derived.auto._
 
       type Cat = (String, Int)
-      forAll { cat: Cat =>
-        checkRoundTrip[Cat](
-          cat,
-          s"""["${cat._1}", ${cat._2}]"""
-        )
+      forAll {
+        cat: Cat =>
+          checkRoundTrip[Cat](
+            cat,
+            s"""["${cat._1}", ${cat._2}]"""
+          )
       }
     }
 
@@ -71,17 +73,19 @@ class DerivedCodecsSpec
       case class Cat(name: String, livesLeft: Int)   extends Animal
       case class Dog(name: String, bonesHidden: Int) extends Animal
 
-      forAll { animal: Cat =>
-        checkRoundTrip[Animal](
-          animal,
-          s"""{"type": "Cat", "name": "${animal.name}", "livesLeft": ${animal.livesLeft}}"""
-        )
+      forAll {
+        animal: Cat =>
+          checkRoundTrip[Animal](
+            animal,
+            s"""{"type": "Cat", "name": "${animal.name}", "livesLeft": ${animal.livesLeft}}"""
+          )
       }: Unit
-      forAll { animal: Dog =>
-        checkRoundTrip[Animal](
-          animal,
-          s"""{"type": "Dog", "name": "${animal.name}", "bonesHidden": ${animal.bonesHidden}}"""
-        )
+      forAll {
+        animal: Dog =>
+          checkRoundTrip[Animal](
+            animal,
+            s"""{"type": "Dog", "name": "${animal.name}", "bonesHidden": ${animal.bonesHidden}}"""
+          )
       }: Unit
     }
 
@@ -94,17 +98,19 @@ class DerivedCodecsSpec
       case class Cat(name: String, livesLeft: Int)   extends Animal
       case class Dog(name: String, bonesHidden: Int) extends Animal
 
-      forAll { animal: Cat =>
-        checkRoundTrip[Animal](
-          animal,
-          s"""{"animalType": "Cat", "name": "${animal.name}", "livesLeft": ${animal.livesLeft}}"""
-        )
+      forAll {
+        animal: Cat =>
+          checkRoundTrip[Animal](
+            animal,
+            s"""{"animalType": "Cat", "name": "${animal.name}", "livesLeft": ${animal.livesLeft}}"""
+          )
       }: Unit
-      forAll { animal: Dog =>
-        checkRoundTrip[Animal](
-          animal,
-          s"""{"animalType": "Dog", "name": "${animal.name}", "bonesHidden": ${animal.bonesHidden}}"""
-        )
+      forAll {
+        animal: Dog =>
+          checkRoundTrip[Animal](
+            animal,
+            s"""{"animalType": "Dog", "name": "${animal.name}", "bonesHidden": ${animal.bonesHidden}}"""
+          )
       }: Unit
     }
 
@@ -115,13 +121,14 @@ class DerivedCodecsSpec
       sealed trait Animal
       case class Cat(name: String, livesLeft: Int) extends Animal
 
-      forAll { `type`: String =>
-        whenever(`type` != "Cat") {
-          val ex = intercept[DeserializationException](s"""{"type": "${`type`}"}""".parseJson.convertTo[Animal])
-          ex.getMessage should ===(
-            s"""failed to decode ${implicitly[ClassTag[Animal]].toString()}: type="${`type`}" is not defined"""
-          )
-        }
+      forAll {
+        `type`: String =>
+          whenever(`type` != "Cat") {
+            val ex = intercept[DeserializationException](s"""{"type": "${`type`}"}""".parseJson.convertTo[Animal])
+            ex.getMessage should ===(
+              s"""failed to decode ${implicitly[ClassTag[Animal]].toString()}: type="${`type`}" is not defined"""
+            )
+          }
       }: Unit
     }
 
@@ -132,14 +139,15 @@ class DerivedCodecsSpec
       sealed trait Animal
       case class Cat(name: String, livesLeft: Int) extends Animal
 
-      forAll { discriminator: String =>
-        whenever(discriminator != "type") {
-          val ex =
-            intercept[DeserializationException](s"""{"$discriminator": "any"}""".parseJson.convertTo[Animal])
-          ex.getMessage should ===(
-            s"""Failed to decode ${implicitly[ClassTag[Animal]].toString()}: discriminator "type" not found"""
-          )
-        }
+      forAll {
+        discriminator: String =>
+          whenever(discriminator != "type") {
+            val ex =
+              intercept[DeserializationException](s"""{"$discriminator": "any"}""".parseJson.convertTo[Animal])
+            ex.getMessage should ===(
+              s"""Failed to decode ${implicitly[ClassTag[Animal]].toString()}: discriminator "type" not found"""
+            )
+          }
       }: Unit
     }
 
@@ -151,11 +159,12 @@ class DerivedCodecsSpec
       case class Leaf(s: String)            extends Tree
       case class Node(lhs: Tree, rhs: Tree) extends Tree
 
-      forAll { tree: Leaf =>
-        checkRoundTrip[Tree](
-          tree,
-          s"""{"type": "Leaf", "s": "${tree.s}"}"""
-        )
+      forAll {
+        tree: Leaf =>
+          checkRoundTrip[Tree](
+            tree,
+            s"""{"type": "Leaf", "s": "${tree.s}"}"""
+          )
       }: Unit
       checkRoundTrip[Tree](
         Node(Node(Leaf("1"), Leaf("2")), Leaf("3")),
