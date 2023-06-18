680db3267c3ba32bf7611b1783f2d4e1203a19bd
Paolo Boni <4746479+paoloboni@users.noreply.github.com>
2023-06-18 00:39:19 +0000

    Revert commit(s) 6d7d797

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,5 +6,5 @@ object Dependencies {
   lazy val scalacheckShapeless = "com.github.alexarchambault" %% "scalacheck-shapeless_1.15" % "1.3.0"
   lazy val scalaTest           = "org.scalatest"              %% "scalatest"                 % "3.2.16"
   lazy val scalacheck_2        = "org.scalatestplus"          %% "scalacheck-1-14"           % "3.2.2.0"
-  lazy val scalacheck_3        = "org.scalatestplus"          %% "scalacheck-1-16"           % "3.2.14.0"
+  lazy val scalacheck_3        = "org.scalatestplus"          %% "scalacheck-1-15"           % "3.2.11.0"
 }
