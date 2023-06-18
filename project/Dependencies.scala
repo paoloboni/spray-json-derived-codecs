a4f2cab3342e8627c0031feb11dd705bbca4539c
Paolo Boni <4746479+paoloboni@users.noreply.github.com>
2023-06-18 00:39:24 +0000

    Update scalacheck-1-15 to 3.2.14.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,6 +5,6 @@ object Dependencies {
   lazy val sprayJson           = "io.spray"                   %% "spray-json"                % "1.3.6"
   lazy val scalacheckShapeless = "com.github.alexarchambault" %% "scalacheck-shapeless_1.15" % "1.3.0"
   lazy val scalaTest           = "org.scalatest"              %% "scalatest"                 % "3.2.16"
-  lazy val scalacheck_2        = "org.scalatestplus"          %% "scalacheck-1-15"           % "3.2.11.0"
-  lazy val scalacheck_3        = "org.scalatestplus"          %% "scalacheck-1-15"           % "3.2.11.0"
+  lazy val scalacheck_2        = "org.scalatestplus"          %% "scalacheck-1-16"           % "3.2.14.0"
+  lazy val scalacheck_3        = "org.scalatestplus"          %% "scalacheck-1-16"           % "3.2.14.0"
 }
